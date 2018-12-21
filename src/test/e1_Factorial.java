022e8e981cd0c0e215716e66377d1fa290d4b969
CY <41314643+ShinCYZhou@users.noreply.github.com>
2018-12-21 23:25:53 +0800

    add error file by YHY

---
+++ src/test/e1_Factorial.java
@@ -0,0 +1,14 @@
+class Factorial{
+    public static void main(String[] a){
+		System.out.println(new Fac().ComputeFac());
+    }
+}
+
+class Fac extends F{ 	// Error
+    public int ComputeFac(int num){
+
+		int a[3];
+
+		return a[3] ;
+    }
+}
